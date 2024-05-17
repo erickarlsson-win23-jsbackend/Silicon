f83b7d20906f10258be81c65a96e4a598ae20832
Eric Karlsson <erickarlsson.ek@gmail.com>
2024-05-17 13:24:54 +0200

    Add project files.

---
+++ BlazorApp/BlazorApp/Helpers/CheckboxRequired.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Helpers;
+
+public class CheckboxRequired : ValidationAttribute
+{
+    public override bool IsValid(object? value) => value is bool b && b;
+}
+
---
+++ BlazorApp/BlazorApp/Models/AccountDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.Models;
+
+public class AccountDetails
+{
+    public string Title { get; set; } = "Account Details";
+
+    public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel();
+
+    public AccountDetailsAddressInfoModel AddressInfo { get; set; } = new AccountDetailsAddressInfoModel();
+}
